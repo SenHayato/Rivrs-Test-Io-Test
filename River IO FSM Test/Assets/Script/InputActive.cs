d7579fe2ed2b4e12122e98a3ec6a0a4891d9f237
SenHayato <157812043+SenHayato@users.noreply.github.com>
2026-04-27 16:32:24 +0700

    Update Input System

--- River IO FSM Test/Assets/Script/InputActive.cs
+++ River IO FSM Test/Assets/Script/InputActive.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputActive : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static InputActive instance;
+    [SerializeField] PlayerInput playerInput;
+
+    public InputAction moveAction, attackAction, jumpAction;
+
+    private void Awake()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        playerInput = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        moveAction = playerInput.actions.FindAction("Move");
+        attackAction = playerInput.actions.FindAction("Attack");
+        jumpAction = playerInput.actions.FindAction("Jump");
     }
+
+    //void Update()
+    //{
+    //    if (moveAction.triggered)
+    //    {
+    //        Debug.Log("Move Input");
+    //    }
+
+    //    if (attackAction.triggered)
+    //    {
+    //        Debug.Log("Attack Input");
+    //    }
+
+    //    if (jumpAction.triggered)
+    //    {
+    //        Debug.Log("Jump Input");
+    //    }
+    //}
 }
