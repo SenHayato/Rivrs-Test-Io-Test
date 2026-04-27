f7deea396e0fed699c081a8dbbe0fe7b9c89bebd
SenHayato <157812043+SenHayato@users.noreply.github.com>
2026-04-27 16:56:37 +0700

    Update Animation dan State

--- River IO FSM Test/Assets/Script/PlayerScript/NinjaState.cs
+++ River IO FSM Test/Assets/Script/PlayerScript/NinjaState.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 public class NinjaState : MonoBehaviour
 {
+    public static NinjaState Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
